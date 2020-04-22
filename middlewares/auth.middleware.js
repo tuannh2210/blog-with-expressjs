fed841bd59a086fa660cdf5d0ad79aaa87aca152
tuannh2210 <tuannh2210@gmail.com>
2020-04-22 21:14:52 +0700

    Update code

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -1,11 +1,11 @@
 module.exports = {
   ensureAuthenticated: (req, res, next) => {
-    if (!req.isAuthenticated()) {
+    if (req.isAuthenticated()) {
+      res.locals.user = req.user;
+      next();
+    } else {
       req.flash('error_msg', 'Please login');
       res.redirect('/login');
-    } else {
-      res.locals.user = req.user;
-      return next();
     }
-  }
+  },
 };
